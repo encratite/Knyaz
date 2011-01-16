6416caa9af52eb97948115e268cee4a6df20a063
void <git.email.address@googlemail.com>
2011-01-16 21:47:29 +0100

    Started working on the synchronised console IO

---
+++ Source/Knyaz/Console.hs
@@ -0,0 +1,23 @@
+module Console where
+
+import Control.Concurrent
+import Control.Monad.Reader
+
+data LockedConsoleState = LockedConsoleState {
+  consoleLock :: MVar ()
+  }
+
+type LockedConsole = ReaderT LockedConsoleState
+
+lockedPrint :: String -> LockedConsole IO ()
+lockedPrint string = do
+  lock <- asks consoleLock
+  liftIO $ do
+    takeMVar lock
+    putStr string
+    putMVar lock ()
+
+runLockedConsole :: MonadIO m => LockedConsole m a -> m a
+runLockedConsole action = do
+  lock <- liftIO $ newMVar ()
+  runReaderT action $ LockedConsoleState lock
