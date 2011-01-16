58fcd69a7c045d359dc7a3d04b59e0e8edb3fb52
void <git.email.address@googlemail.com>
2011-01-16 22:50:21 +0100

    Added exports and functions for printing without and with a newline

--- Source/Knyaz/Console.hs
+++ Source/Knyaz/Console.hs
@@ -1,4 +1,8 @@
-module Knyaz.Console where
+module Knyaz.Console(
+  lockedPrintString,
+  lockedPrint,
+  runLockedConsole
+  ) where
 
 import Control.Concurrent
 import Control.Monad.Reader
@@ -9,13 +13,16 @@ data LockedConsoleState = LockedConsoleState {
 
 type LockedConsole = ReaderT LockedConsoleState
 
-lockedPrint :: String -> LockedConsole IO ()
-lockedPrint string = do
+lockedPrintString :: String -> LockedConsole IO ()
+lockedPrintString string = do
   lock <- asks consoleLock
   liftIO $ do
     takeMVar lock
     putStr string
     putMVar lock ()
+
+lockedPrint :: String -> LockedConsole IO ()
+lockedPrint string = lockedPrintString $ string ++ "\n"
 
 runLockedConsole :: MonadIO m => LockedConsole m a -> m a
 runLockedConsole action = do
