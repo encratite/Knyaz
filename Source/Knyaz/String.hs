9cae0e2b497b639a334bbc9f7eb6b9e665499f2e
void <git.email.address@googlemail.com>
2011-01-22 16:50:01 +0100

    Whitespace formatting fix

--- Source/Knyaz/String.hs
+++ Source/Knyaz/String.hs
@@ -6,18 +6,20 @@ module Knyaz.String(
 readMaybe :: Read a => String -> Maybe a
 readMaybe string =
   let results = reads string in
-      if null results then
-        -- failed to parse anything
-        Nothing
-      else
-        -- managed to parse at least one object
-        let readResult = head results
-            object = fst readResult
-            remainingString = snd readResult
-            in
-         if null remainingString then
-           -- successfully parsed the entire string
-           Just object
-         else
-           -- the string was not processed fully
-           Nothing
+  if null results
+  then
+    -- failed to parse anything
+    Nothing
+  else
+    -- managed to parse at least one object
+    let readResult = head results
+        object = fst readResult
+        remainingString = snd readResult
+    in
+     if null remainingString
+     then
+       -- successfully parsed the entire string
+       Just object
+     else
+       -- the string was not processed fully
+       Nothing
