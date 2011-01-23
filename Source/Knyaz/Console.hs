718ee3b30b27c741db5772100cde1288a0f79178
void <git.email.address@googlemail.com>
2011-01-23 21:54:49 +0100

    Made the signatures less redundant

--- Source/Knyaz/Console.hs
+++ Source/Knyaz/Console.hs
@@ -14,13 +14,15 @@ data LockedConsoleState = LockedConsoleState {
 
 type LockedConsole = ReaderT LockedConsoleState
 
+type PrintFunction m = String -> LockedConsole m ()
+
 withLockedConsole :: MonadIO m => LockedConsole m a -> m a
 withLockedConsole body = do
   lock <- liftIO $ newMVar ()
   let consoleState = LockedConsoleState lock
   runReaderT body consoleState
 
-printer :: MonadIO m => (String -> IO ()) -> String -> LockedConsole m ()
+printer :: MonadIO m => (String -> IO ()) -> PrintFunction m
 printer printFunction string = do
   lock <- asks consoleLock
   liftIO $ do
@@ -28,8 +30,8 @@ printer printFunction string = do
     printFunction string
     putMVar lock ()
 
-printString :: MonadIO m => String -> LockedConsole m ()
+printString :: MonadIO m => PrintFunction m
 printString = printer putStr
 
-printLine :: MonadIO m => String -> LockedConsole m ()
+printLine :: MonadIO m => PrintFunction m
 printLine = printer putStrLn
