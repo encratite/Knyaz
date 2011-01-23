3348f95f577491b93a378702e3b9f958b6149c60
void <git.email.address@googlemail.com>
2011-01-23 16:05:34 +0100

    Replaced the old TVar printer thread code with a monadic MVar lock based approach

--- Source/Knyaz/Console.hs
+++ Source/Knyaz/Console.hs
@@ -1,56 +1,35 @@
 module Knyaz.Console(
-  PrintFunction,
-  withLockedPrinting,
-  withLockedLinePrinting
+  LockedConsole,
+  withLockedConsole,
+  printString,
+  printLine
   ) where
 
-import Control.Concurrent hiding (forkIO)
-import Control.Concurrent.STM
-import Control.ContStuff
+import Control.Concurrent.MVar
+import Control.Monad.Reader
 
-type PrintFunction = String -> IO ()
-type PrintStrings = [String]
+data LockedConsoleState = LockedConsoleState {
+  consoleLock :: MVar ()
+  }
 
-type TerminationAction = IO ()
+type LockedConsole = ReaderT LockedConsoleState
 
-data PrinterCommand =
-  -- arguments: function which handles the printing, strings to print
-  PrintString PrintFunction PrintStrings |
-  -- this takes an IO action which is responsible for writing a TVar to indicate the termination of the thread
-  QuitPrinting TerminationAction
+withLockedConsole :: MonadIO m => LockedConsole m a -> m a
+withLockedConsole body = do
+  lock <- liftIO $ newMVar ()
+  let consoleState = LockedConsoleState lock
+  runReaderT body consoleState
 
--- | Takes a an MVar which transmits the printer commands from the printing environment to the printing thread.
--- Returns a function which is used for the actual transmission.
-lockedPrinter :: MVar PrinterCommand -> IO (PrinterCommand -> IO ())
-lockedPrinter synchronisedCommand = do
-  void . forkIO . evalContT . forever $ do
-    command <- liftIO $ takeMVar synchronisedCommand
-    case command of
-         PrintString printer strings -> liftIO $ mapM_ printer strings
-         QuitPrinting action -> do
-           liftIO action
-           abort ()
-  return $ putMVar synchronisedCommand
+printer :: MonadIO m => (String -> IO ()) -> String -> LockedConsole m ()
+printer printFunction string = do
+  lock <- asks consoleLock
+  liftIO $ do
+    takeMVar lock
+    printFunction string
+    putMVar lock ()
 
+printString :: MonadIO m => String -> LockedConsole m ()
+printString = printer putStr
 
--- | Creates an environment for synchronised console output.
--- Takes a body to execute which takes two functions:
--- 1. one for printing without a newline
--- 2. one for printing with a newline appended at the end
-withLockedPrinting :: (PrintFunction -> PrintFunction -> IO ()) -> IO ()
-withLockedPrinting body = do
-  synchronisedCommand <- newEmptyMVar
-  printer <- lockedPrinter synchronisedCommand
-  let printFunctionWrapper function string = printer $ PrintString function [string]
-      printString = printFunctionWrapper putStr
-      printLine = printFunctionWrapper putStrLn
-  body printString printLine
-  quitTransaction <- newTVarIO False
-  printer $ QuitPrinting (atomically $ writeTVar quitTransaction True)
-  atomically $ readTVar quitTransaction >>= check
-
--- | A less generic version of withLockedPrinting which only provides a function to print entire lines.
-withLockedLinePrinting :: (PrintFunction -> IO ()) -> IO ()
-withLockedLinePrinting body =
-  withLockedPrinting customBody
-  where customBody _ printLine = body printLine
+printLine :: MonadIO m => String -> LockedConsole m ()
+printLine = printer putStrLn
