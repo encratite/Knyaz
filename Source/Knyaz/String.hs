62f5040dd61dd4e3d956ba94d17bd25f24147fa9
void <git.email.address@googlemail.com>
2011-02-07 22:44:50 +0100

    Added some new functions for trimming and tokenising to the String module

--- Source/Knyaz/String.hs
+++ Source/Knyaz/String.hs
@@ -1,6 +1,15 @@
 module Knyaz.String(
-  readMaybe
+  readMaybe,
+  leftTrim,
+  rightTrim,
+  trimString,
+  tokeniseByteString,
+  tokeniseString
   ) where
+
+import Data.Char
+import qualified Data.ByteString as DB
+import qualified Data.ByteString.UTF8 as DBU
 
 -- | Attempt to convert an entire string to the desired type which is an instance of Read.
 readMaybe :: Read a => String -> Maybe a
@@ -23,3 +32,34 @@ readMaybe string =
      else
        -- the string was not processed fully
        Nothing
+
+-- | Trim the whitespace off the left side of a string.
+leftTrim :: String -> String
+leftTrim = dropWhile isSpace
+
+-- | Trim the whitespace off the right side of a string.
+rightTrim :: String -> String
+rightTrim = reverse . leftTrim . reverse
+
+-- | Trim the whitespace off both the left and the right side of a string.
+trimString :: String -> String
+trimString = leftTrim . rightTrim
+
+-- | Tokenise a bytestring.
+tokeniseByteString :: DB.ByteString -> DB.ByteString -> [DB.ByteString]
+tokeniseByteString string delimiter =
+  collected : if DB.null remaining
+              then []
+              else otherTokens
+  where
+    (collected, remaining) = DB.breakSubstring delimiter string
+    otherTokens =
+      let withoutDelimiter = DB.drop (DB.length delimiter) remaining in
+      tokeniseByteString withoutDelimiter delimiter
+
+-- | Tokenise a string.
+tokeniseString :: String -> String -> [String]
+tokeniseString string delimiter =
+  map DBU.toString $ tokeniseByteString (byteString string) (byteString delimiter)
+  where
+    byteString = DBU.fromString
